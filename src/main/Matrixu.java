98d712fb27cce41eb41db63fd42c9ff5c5f80afa
Guillen Tom <tom.lefils@gmail.com>
2020-02-26 12:21:51 +0100

    question2.0

---
+++ src/main/Matrixu.java
@@ -0,0 +1,23 @@
+public class Matrixu {
+    private int a;
+    private int b;
+    private int m;
+    private int n;
+
+    public Matrixu(int a, int b, int m, int n){
+        this.a = a;
+        this.b = b;
+        this.m = m;
+        this.n = n;
+    }
+
+    public int[][] buildMatrix(){
+        int[] row = new int[this.m];
+        for(int i=0; i< m; i++){
+            //TODO
+        }
+
+        return null;
+    }
+
+ }
